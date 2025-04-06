37e8ca01b66d0535a45599ab07326e46839b9de5
TruthGuard Deployer <ab9604783@gmail.com>
2025-04-06 16:28:13 +0530

    CORS FIXED

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.get('/', (req, res) => {
 // CORS setup with appropriate configuration for different environments
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL 
-    : ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500' , '*'],
+    ? [process.env.FRONTEND_URL, 'http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000']
+    : '*',
   credentials: true,
   optionsSuccessStatus: 200
 };
